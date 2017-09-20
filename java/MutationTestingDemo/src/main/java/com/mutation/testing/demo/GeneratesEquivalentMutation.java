ba9e7f6a78ca37ce54f6b13319472ac59d1305ce
James White <j_n_white@hotmail.com>
2017-09-20 11:34:14 +0100

    Added equivalent mutation example

--- java/MutationTestingDemo/src/main/java/com/mutation/testing/demo/GeneratesEquivalentMutation.java
+++ java/MutationTestingDemo/src/main/java/com/mutation/testing/demo/GeneratesEquivalentMutation.java
@@ -3,7 +3,11 @@ package com.mutation.testing.demo;
 import java.util.Collection;
 import java.util.Optional;
 
-public class GeneratesEquivalentMutation {
+/**
+ * When "lowest.compareTo(i) > 0" is mutated to "lowest.compareTo(i) >= 0" the same value will still be returned so the
+ * code will still behave in the same way meaning the mutation will not get killed by any tests.
+ */
+class GeneratesEquivalentMutation {
 
     Optional<Integer> getLowestValue(Collection<Integer> input) {
         Integer lowest = null;
